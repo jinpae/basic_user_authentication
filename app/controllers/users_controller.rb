749274e5ec842f9aa3ca867c7d72a46da7673d4d
Jin Pae <jin@jinpae.com>
2015-05-03 15:51:53 +0900

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to root_url, success: "Your account has been created successfully!"
+		else
+			render :new
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
 end
