1b09c00537df957ae5a13464618e998e634319f0
Jin Pae <jin@jinpae.com>
2015-05-03 17:14:19 +0900

    Manage user sessions via session hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 
 		if user && user.authenticate(params[:password])
+			# store user's id in a temporary session
+			session[:user_id] = user.id
 			redirect_to user, success: "Signed in successfully"
 		else
 			flash.now[:warning] = "Invalid email or password"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
-			redirect_to root_url, success: "Your account has been created successfully!"
+			session[:user_id] = @user.id
+			redirect_to @user, success: "Your account has been created successfully!"
 		else
 			render :new
 		end
