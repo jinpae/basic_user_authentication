53e63ad1d1880b1580e7100a8b2e9dd8e84aeaee
Jin Pae <jin@jinpae.com>
2015-05-03 16:24:29 +0900

    Add form validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
 end
