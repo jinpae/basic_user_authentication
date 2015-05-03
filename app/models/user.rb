94d0ab0d27fe3579daec4df35c2cd655e6ac8862
Jin Pae <jin@jinpae.com>
2015-05-03 17:32:46 +0900

    Refactor user authentication logic from sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
+
+	def self.authenticate(email, password)
+		user = find_by(email: email)
+		user && user.authenticate(password)
+	end
 end
