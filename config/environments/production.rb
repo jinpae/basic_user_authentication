200e30680a07e7296247aaa0ab49cd4d068a8126
Jin Pae <jin@jinpae.com>
2015-05-05 23:48:19 +0900

    Update mailer configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,11 @@ Rails.application.configure do
 	config.action_mailer.delivery_method = :smtp
 
 	config.action_mailer.smtp_settings = {
-		address: "smtp.gmail.com",
-		port: 587,
-		domain: "example.com",
-		authentication: "plain",
-		enable_starttls_auto: true,
-		user_name: Rails.application.secrets.gmail_username,
-		password: Rails.application.secrets.gmail_password
+		port: '587',
+		address: 'smtp.mandrillapp.com',
+		user_name: ENV['MANDRILL_USERNAME'],
+		password: ENV['MANDRILL_APIKEY'],
+		domain: "heroku.com",
+		authentication: :plain,
 	}
 end
